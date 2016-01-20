7e96edad322c1d990120a9308e7f8292b16115ac
peteratseneca <peter.mcintyre@senecacollege.ca>
2016-01-20 05:55:01 -0500

    Persistent store tasks - get all, get one

---
+++ Week_02/GetAllGetOne/GetAllGetOne/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetAllGetOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Reference to the data manager
+        private Manager m = new Manager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
