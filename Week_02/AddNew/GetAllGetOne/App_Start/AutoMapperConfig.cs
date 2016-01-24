204d65d424ab5ac2b896afaca4beb47656b3083f
peteratseneca <peter.mcintyre@senecacollege.ca>
2016-01-24 17:09:20 -0500

    Persistent store tasks - get all, get one, add new

---
+++ Week_02/AddNew/GetAllGetOne/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+
+namespace GetAllGetOne
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // Add map creation statements here
+            // Mapper.CreateMap< FROM , TO >();
+
+            Mapper.CreateMap<Models.Customer, Controllers.CustomerBase>();
+
+            Mapper.CreateMap<Controllers.CustomerAdd, Models.Customer>();
+        }
+    }
+}
