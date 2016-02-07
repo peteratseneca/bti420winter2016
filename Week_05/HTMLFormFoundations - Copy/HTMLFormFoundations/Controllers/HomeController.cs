8c51a05953b1a3e1af41541d449c99b5888509f4
peteratseneca <peter.mcintyre@senecacollege.ca>
2016-02-07 12:06:38 -0500

    Associations and HTML Forms

---
+++ Week_05/HTMLFormFoundations - Copy/HTMLFormFoundations/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLFormFoundations.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleInput()
+        {
+            return View();
+        }
+
+        public ActionResult DropdownListboxSimple()
+        {
+            // Create a collection of strings
+            // They will be rendered in an item-selection control
+            var colourNames = new List<string> { "Red", "Green", "Blue" };
+
+            // Pass the collection to the view
+            // At the top of the view, add a statement to declare the model/type
+            // @model IEnumerable<string>
+            return View(colourNames);
+        }
+
+        public ActionResult DropdownListboxObject()
+        {
+            // Create a collection of "Teacher" objects (the class is defined below)
+            var teachers = new List<Teacher>
+            {
+                new Teacher { Id = 1, Name = "Peter McIntyre", Age = 39 },
+                new Teacher { Id = 2, Name = "Ian Tipson", Age = 42 },
+                new Teacher { Id = 3, Name = "Wei Song", Age = 35 }
+            };
+
+            // Pass the collection to the view
+            // At the top of the view, add a statement to declare the model/type
+            // @model IEnumerable<HTMLFormFoundations.Controllers.Teacher>
+            return View(teachers);
+        }
+
+        public ActionResult RadioCheckSimple()
+        {
+            // Create a collection of strings
+            // They will be rendered in an item-selection control
+            var colourNames = new List<string> { "Red", "Green", "Blue" };
+
+            // Pass the collection to the view
+            // At the top of the view, add a statement to declare the model/type
+            // @model IEnumerable<string>
+            return View(colourNames);
+        }
+
+        public ActionResult RadioCheckObject()
+        {
+            // Create a collection of "Teacher" objects (the class is defined below)
+            var teachers = new List<Teacher>
+            {
+                new Teacher { Id = 1, Name = "Peter McIntyre", Age = 39 },
+                new Teacher { Id = 2, Name = "Ian Tipson", Age = 42 },
+                new Teacher { Id = 3, Name = "Wei Song", Age = 35 }
+            };
+
+            // Pass the collection to the view
+            // At the top of the view, add a statement to declare the model/type
+            // @model IEnumerable<HTMLFormFoundations.Controllers.Teacher>
+            return View(teachers);
+        }
+    }
+
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
---
+++ Week_05/README.md
@@ -0,0 +1,17 @@
+### Week 5 code examples
+
+**HTMLFormFoundations**
+
+Introduces you to the foundations for building rich HTML Forms.  
+
+Features:
+- simple input
+- item-selection controls (dropdown, listbox, radio button group, checkbox group)
+
+**SelectListIntro**
+
+Builds upon the ideas in the previous code example.  
+
+Features:
+- package data for an item-selection control into a SelectList object
+- covers some new-to-you HTML Helpers
